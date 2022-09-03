287f2bd914e18cb45d564b8f0b4ef204e5d1f285
James Mburu Githire <jamesmburug24@gmail.com>
2022-09-03 18:33:59 +0300

    Fonts and other changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Info from './Info';
 
 function App() {
   return (
     <div className="App">
       <h1>Testing application</h1>
+      <Info/>
     </div>
   );
 }
-
 export default App;
---
+++ src/Info.js
@@ -0,0 +1,8 @@
+import photo from './logo.svg';
+export default function Info() {
+    return (
+        <div>
+            <img src={photo} className="photo" alt="photo"/>
+        </div>
+    )
+}
